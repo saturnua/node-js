e2cd5a6593751039bca56b59276a1b1a04ef3d3f
Ievhen Perekhodchenko <i.perekhodchenko@favbet.com>
2021-01-07 14:03:23 +0200

    WIP: finish session functionality

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
 const express = require('express')
 const path = require('path')
+const csrf = require('csurf')
+const flash = require('connect-flash')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Handlebars = require('handlebars')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
+const varMiddleware = require('./middleware/variables')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
-const User = require('./models/user')
+const authRoutes = require('./routes/auth')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
-
+const MONGODB_URI = `mongodb+srv://inecon:5yMDdlzmgpl6pJQB@cluster0.1i9bl.mongodb.net/shop`
+const userMiddleware = require('./middleware/user')
 
 const app = express()
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs',
-  handlebars: allowInsecurePrototypeAccess(Handlebars)
+    defaultLayout: 'main',
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MONGODB_URI,
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('5fe873619faf766b5db2f618')
-    req.user = user
-    next()
-  } catch (e) {
-    console.log(e)
-  }
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
+app.use(session({
+    secret: 'some secret value',
+    resave: false,
+    saveUninitialized: false,
+    store
+}))
+app.use(csrf(session))
+app.use(flash())
+app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 3000
 
 async function start() {
-  try {
-    const url = `mongodb+srv://inecon:5yMDdlzmgpl6pJQB@cluster0.1i9bl.mongodb.net/shop`
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    })
-    const candidate = await User.findOne()
-    if (!candidate) {
-      const user = new User({
-        email: 'saturnua@gmail.com',
-        name: 'Saturnua',
-        cart: {items: []}
-      })
-      await user.save()
-      //del before prod
-      console.log(user)
+    try {
+        const url = MONGODB_URI
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
     }
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-    })
-  } catch (e) {
-    console.log(e)
-  }
 }
 
 start()
--- routes/card.js
+++ routes/card.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const Course = require('../models/course')
+const auth = require('../middleware/auth')
 const router = Router()
 
 
@@ -17,7 +18,7 @@ function computePrice(courses) {
   }, 0)
 }
 
-router.post('/add', async (req, res) => {
+router.post('/add', auth, async (req, res) => {
   try {
     const course = await Course.findById(req.body.id)
     await req.user.addToCart(course)
@@ -28,7 +29,7 @@ router.post('/add', async (req, res) => {
   }
 })
 
-router.delete('/remove/:id', async (req, res) => {
+router.delete('/remove/:id', auth, async (req, res) => {
   await req.user.removeFromCart(req.params.id)
   const user = await req.user.populate('cart.items.courseId').execPopulate()
   const courses = mapCartItems(user.cart)
@@ -38,7 +39,7 @@ router.delete('/remove/:id', async (req, res) => {
   res.status(200).json(cart)
 })
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   const user = await req.user
     .populate('cart.items.courseId')
     .execPopulate()
